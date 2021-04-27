097243784997aac59f45ffbe7c88637df831a8d2
executeinstaller <executeinstaller@hisownlaptop>
2021-04-27 19:53:08 +0300

    0.0.4

--- prozorro_api/__init__.py
+++ prozorro_api/__init__.py
@@ -3,11 +3,10 @@ import multiprocessing as mp
 from retry import retry
 import functools
 
-
 API_URL = "https://public.api.openprocurement.org/api/2.5"
 
-     
-@retry(Exception, tries = 420, delay = 15, backoff=1.5, max_delay = 300)
+
+@retry(Exception, tries=420, delay=15, backoff=1.5, max_delay=300)
 def _get_objects_page(name, offset):
     """
     Витягти одну сторінку планів (100 штук)
@@ -15,11 +14,12 @@ def _get_objects_page(name, offset):
 
     r = requests.get(f"{API_URL}/{name}?offset={offset}", timeout=15)
     page = r.json()
-    
+
     next_page = page["next_page"]
     items = page["data"]
 
     return next_page, items
+
 
 def _get_objects_gen(name, start_offset, end_offset):
     """
@@ -30,12 +30,12 @@ def _get_objects_gen(name, start_offset, end_offset):
 
     while next_offset < end_offset:
         next_page, items = _get_objects_page(name, next_offset)
-        yield from items    
+        yield from items
 
         next_offset = next_page["offset"]
 
 
-@retry(Exception, tries = 420, delay = 15, backoff=1.5, max_delay = 300)
+@retry(Exception, tries=420, delay=15, backoff=1.5, max_delay=300)
 def _get_object(name, id):
     r = requests.get(f"{API_URL}/{name}/{id}", timeout=15)
     page = r.json()
@@ -54,7 +54,7 @@ def get_objects_stream(name, start_offset, end_offset):
 
     id_stream = _get_objects_gen(name, start_offset, end_offset)
     task = functools.partial(_get_obj_by_def, name=name)
- 
+
     yield from pool.imap(task, id_stream)
 
 
